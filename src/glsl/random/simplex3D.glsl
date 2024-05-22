5a06088c9fae20784656246c0a73ecee5d7b4e67
Pierre Keda <pierrekeda83@gmail.com>
2024-05-22 11:02:21 +0200

    Fix simplex3D typo

--- src/glsl/random/simplex3D.glsl
+++ src/glsl/random/simplex3D.glsl
@@ -75,7 +75,7 @@ float simplex3D(vec3 v) {
   vec4 m = max(0.6 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
   m = m * m;
 
-  return 42.0 * dot( m*m, vec4(dot(p0,x0), dot(p1,x1), dot(p2,x2), dot(p3,x3));
+  return 42.0 * dot(m*m, vec4(dot(p0,x0), dot(p1,x1), dot(p2,x2), dot(p3,x3)));
 }
 
 float simplex3D(float x, float y, float z) {
